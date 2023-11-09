4d0b2af7a7264d21c57db5fd7a8aa8900044b265
Pep Vilarós <vilarospro@gmail.com>
2023-11-10 00:22:49 +0100

    Subscribe the dot to the mouse position with useffect and a when the button is clicked with useState.

--- projects/02-mouse-follower/src/App.jsx
+++ projects/02-mouse-follower/src/App.jsx
@@ -1,8 +1,34 @@
+import { useState, useEffect } from 'react'
+
 function App() {
+
+  // States
+  const [enabled, setEnabled] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  // Effects
+  useEffect(() => {
+
+    const handleMove = (event) => {
+      setPosition({ x: event.clientX, y: event.clientY });
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => {
+      console.log('clean up')
+      window.removeEventListener('pointermove', handleMove);
+    }
+    
+  }, [enabled]);
 
   return (
     <>
       <h1>Mouse Follower Project</h1>
+      <div className="dot" style={{transform: `translate(${position.x}px, ${position.y}px)`}}></div>
+      <button onClick={() => setEnabled(!enabled)}>{enabled ? 'Deactivate' : 'Activate'} the mouse follower!</button>
     </>
   )
 }
